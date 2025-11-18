a703e1ec8fcbc1da6b73280a6ceab09aecd171c8
Yuto Ichinose <60034520+illionillion@users.noreply.github.com>
2025-11-18 14:41:00 +0000

    fix: closes nextの公開 Fixes #3

--- docs/assets/jsx-runtime-Bha7v8PO.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./iframe-DI8GWPo7.js";var s=r();export{s as j};
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  distDir: process.env.NODE_ENV === "production" ? "docs" : ".next",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === "production" ? "/storybook-vrt-sample" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/storybook-vrt-sample/" : "",
 };
 
 export default nextConfig;
